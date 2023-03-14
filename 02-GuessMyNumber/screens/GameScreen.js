14c84f0724a5e6be304b710ca100a537f1b7a3cf
Maximiliano Cóppola <maxicopp@outlook.com>
2023-03-14 03:01:53 -0300

    Improving the Game Screen Visuals

--- 02-GuessMyNumber/screens/GameScreen.js
+++ 02-GuessMyNumber/screens/GameScreen.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Alert, StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 import NumberContainer from '../components/game/NumberContainer';
+import Card from '../components/ui/Card';
+import InstructionText from '../components/ui/InstructionText';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
 
@@ -54,8 +56,8 @@ function GameScreen({ userNumber, onGameOver }) {
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
-      <View>
-        <Text>Higher or lower?</Text>
+      <Card>
+        <InstructionText>Higher or lower?</InstructionText>
         <View>
           <PrimaryButton onPress={nextGuessHanlder.bind(this, 'lower')}>
             -
@@ -64,7 +66,7 @@ function GameScreen({ userNumber, onGameOver }) {
             +
           </PrimaryButton>
         </View>
-      </View>
+      </Card>
       {/* <View>LOG ROUNDS</View> */}
     </View>
   );
