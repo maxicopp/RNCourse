466543e2538af17a30287fa315e56b4300eb6409
Maximiliano Cóppola <maxicopp@outlook.com>
2023-04-07 17:22:28 -0300

    Displaying Meals

--- 03-MealsApp/screens/MealsOverviewScreen.js
+++ 03-MealsApp/screens/MealsOverviewScreen.js
@@ -1,11 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
+  const displayedMeals = MEALS.filter(
+    (mealItem) => mealItem.categoryIds.indexOf(catId) >= 0
+  );
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
