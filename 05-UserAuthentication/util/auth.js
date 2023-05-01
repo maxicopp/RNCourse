7c54bd01bc20144aa64e514addbb3f9e620468c2
Maximiliano Cóppola <maxicopp@outlook.com>
2023-04-30 22:38:42 -0300

    Sending Authentication Requests to the Backend

---
+++ 05-UserAuthentication/util/auth.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = 'AIzaSyBVMUT0D_l2ZPAiWwGae292b8eWwpbnsQE';
+
+async function createUser(email, password) {
+  const response = await axios.post(
+    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+    {
+      email,
+      password,
+      returnSecureToken: true,
+    }
+  );
+}
