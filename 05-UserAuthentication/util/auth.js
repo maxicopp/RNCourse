8d1feb6942b2fe91196f0cff63bc5ec86e8799a5
Maximiliano Cóppola <maxicopp@outlook.com>
2023-04-30 22:53:56 -0300

    Creating New Users

--- 05-UserAuthentication/util/auth.js
+++ 05-UserAuthentication/util/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyBVMUT0D_l2ZPAiWwGae292b8eWwpbnsQE';
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
     {
