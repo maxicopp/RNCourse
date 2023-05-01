9470ece5b02aed792e3e2a7f118d11660129d07b
Maximiliano Cóppola <maxicopp@outlook.com>
2023-04-30 23:09:02 -0300

    Logging Users In

--- 05-UserAuthentication/util/auth.js
+++ 05-UserAuthentication/util/auth.js
@@ -2,13 +2,21 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyBVMUT0D_l2ZPAiWwGae292b8eWwpbnsQE';
 
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  const response = await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+}
+
 export async function createUser(email, password) {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    {
-      email,
-      password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate('signUp', email, password);
+}
+
+export async function login(email, password) {
+  await authenticate('signInWithPassword', email, password);
 }
