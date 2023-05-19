293dd7f639901258335086dac167a62ed33a4d75
Maximiliano Cóppola <maxicopp@outlook.com>
2023-05-19 01:40:46 -0300

    Using native device features with the bare workflow

---
+++ 07-RNCourseBare/App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
+
+export default function App() {
+  async function getUserLocationHandler() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Get Location" onPress={getUserLocationHandler} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
